8656cda1a8676bc2ae91c00766ce5ab79f14f31f
Jonathan Tweedy <jetweedy@users.noreply.github.com>
2020-11-12 14:36:51 -0500

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -2,15 +2,23 @@
 
 // Connect to the database
 
+/*
 $hostname = "localhost";
 $username = "dlargent";
 $password = "asdfjkl";
 $db = "workouts";
+*/
 
-$dbconnect=mysqli_connect($hostname,$username,$password,$db);
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+$dbconnect=mysqli_connect($server,$username,$password,$db);
 
 if ($dbconnect->connect_error) {
   die("Database connection failed: " . $dbconnect->connect_error);
 }
+
 
 ?>
